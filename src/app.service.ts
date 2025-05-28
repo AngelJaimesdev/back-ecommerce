07daad3d1973f624192f24ba8495b926ccd3322d
AngelJaimesdev <manuelangelcr7@gmail.com>
2025-05-28 16:04:54 -0500

    creacion controlador para generar token

--- src/app.service.ts
+++ src/app.service.ts
@@ -5,4 +5,11 @@ export class AppService {
   getHello(): string {
     return 'Hello World!';
   }
+
+  generateToken() {
+    const credential = {
+      client: '7w7OVivkvY0nBFZ2-u0g',
+      secret: '4FTEE5GkLnsAiNehAm-z',
+    };
+  }
 }
