030ea053c3ab92964d170937c669f09b3a02b617
AngelJaimesdev <manuelangelcr7@gmail.com>
2025-05-29 00:09:39 -0500

    add routes

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:4200',
+     origin: [
+    'http://localhost:4200',
+    'http://127.0.0.1:5501',
+    'http://127.0.0.1:5501/index.html'
+  ],
   });
   await app.listen(3000);
 }
