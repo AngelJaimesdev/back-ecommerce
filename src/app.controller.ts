182715e47d0b3806f46aa6f41072fd17574f7c70
AngelJaimesdev <manuelangelcr7@gmail.com>
2025-05-28 23:01:41 -0500

    fix: add new service

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { firstValueFrom } from 'rxjs';
+import { ConfigService } from '@nestjs/config';
+import { HttpService } from '@nestjs/axios';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService, private configService: ConfigService, private httpService: HttpService) { }
 
   @Get()
   getHello(): string {
@@ -14,4 +17,27 @@ export class AppController {
   generateToken() {
     return this.appService.getToken();
   }
+
+  @Get('proxy-token')
+  async proxyToken() {
+    const url = `${this.configService.get<string>('URL_BACKEND')}/auth-lead/get-token`;
+
+    const body = {
+      credential: {
+        client: this.configService.get<string>('Client'),
+        secret: this.configService.get<string>('Secret'),
+      },
+    };
+
+    try {
+      const response = await firstValueFrom(
+        this.httpService.post(url, body)
+      );
+      return response.data;
+    } catch (error) {
+      console.error('Error en proxy-token:', error.response?.data || error.message);
+      throw error;
+    }
+  }
+
 }
