9bf84e3fffa79a0ba70bc5294d13060d863da68b
AngelJaimesdev <manuelangelcr7@gmail.com>
2025-05-29 09:23:54 -0500

    .

--- src/app.service.ts
+++ src/app.service.ts
@@ -6,9 +6,9 @@ import { firstValueFrom } from 'rxjs';
 
 @Injectable()
 export class AppService {
-   constructor(private configService: ConfigService,
+  constructor(private configService: ConfigService,
     private httpService: HttpService
-   ) {}
+  ) { }
   getHello(): string {
     return 'Hello World!';
   }
@@ -23,19 +23,33 @@ export class AppService {
       }
     };
 
-    console.log('credential', body.credential);
-    
+    console.log('Enviando credenciales:', body.credential);
+
     try {
       const response = await firstValueFrom(
-        this.httpService.post(url, body)
+        this.httpService.post(url, body, {
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          // timeout: 5000,
+        })
       );
-      console.log('log res',response.data);
-      
+
+      console.log('Token recibido:', response.data);
       return response.data;
     } catch (error) {
-      console.error('Error al obtener token:', error.response?.data || error.message);
-      throw error;
+      const status = error.response?.status;
+      const data = error.response?.data;
+
+      console.error('🔴 Error al obtener token:', {
+        status,
+        data,
+        message: error.message,
+      });
+
+      throw new Error(`Fallo al obtener token: ${status || ''} ${data?.message || error.message}`);
     }
   }
-  
+
+
 }
