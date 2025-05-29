b878f410319e997294ac94f65d00ff2bce4b47b0
AngelJaimesdev <manuelangelcr7@gmail.com>
2025-05-29 00:23:52 -0500

    add routes cors

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,8 @@ async function bootstrap() {
      origin: [
     'http://localhost:4200',
     'http://127.0.0.1:5501',
-    'http://127.0.0.1:5501/index.html'
+    'http://127.0.0.1:5501/index.html',
+    'https://ingangeljaimes.wixsite.com/my-site'
   ],
   });
   await app.listen(3000);
