680bd18369e8126aaca45f0af5646b862112a1c0
AngelJaimesdev <manuelangelcr7@gmail.com>
2025-05-29 09:19:54 -0500

    ajustes cors

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,16 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-     origin: [
-    'http://localhost:4200',
-    'http://127.0.0.1:5501',
-    'http://127.0.0.1:5501/index.html',
-    'https://ingangeljaimes.wixsite.com/my-site',
-    'https://ingangeljaimes-wixsite-com.filesusr.com'
-  ],
+  // app.enableCors({
+  //    origin: [
+  //   'http://localhost:4200',
+  //   'http://127.0.0.1:5501',
+  //   'http://127.0.0.1:5501/index.html',
+  //   'https://ingangeljaimes.wixsite.com/my-site',
+  //   'https://ingangeljaimes-wixsite-com.filesusr.com'
+  // ],
+   app.enableCors({
+    origin: '*',
   });
   await app.listen(3000);
 }
