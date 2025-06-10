1e9ed680bd2287ba5e31e5112eac4e1ecdf5045c
AngelJaimesdev <manuelangelcr7@gmail.com>
2025-06-10 14:52:20 -0500

    cambios

--- src/app.service.ts
+++ src/app.service.ts
@@ -14,8 +14,9 @@ export class AppService {
     return 'Hello World!';
   }
 
-  async getToken() {
-    const url = `${this.configService.get<string>('URL_BACKEND')}/auth-bop/get-token`;
+  async getToken(): Promise<any> {
+      const url = `${this.configService.get<string>('URL_BACKEND')}/auth-bop/get-token`;
+
 
     const body = {
       credential: {
@@ -39,42 +40,102 @@ export class AppService {
             quantity: 1,
           },
         ],
-        callbackUrl: 'rrrrr',
-        redirectionUrl: 'tttttt',
+        callbackUrl: 'https://x.com/home',
+        redirectionUrl: 'https://www.youtube.com/',
       },
     };
 
     console.log('Enviando credenciales:', body.credential);
     console.log('URL:', url);
+    
+    
 
     try {
-      const response = await firstValueFrom(
-        this.httpService.post(url, body, {
-          headers: {
-            'Content-Type': 'application/json',
-            'Accept': 'application/json',
-          },
-          timeout: 8000,
-        }),
-      );
-
-      console.log('Token recibido:', response.data);
-      return response.data;
-    } catch (error) {
-      const status = error.response?.status;
-      const data = error.response?.data;
-
-      console.error('Error al obtener token:', {
-        status,
-        data,
-        message: error.message,
+      const response = await fetch(url, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(body),
       });
 
-      throw new Error(
-        `Fallo al obtener token: ${status || ''} ${
-          data?.message || error.message
-        }`,
-      );
+      if (!response.ok) {
+        const errorData = await response.json();
+        console.error('Error al obtener el token:', errorData);
+        throw new Error(`HTTP error ${response.status}: ${errorData.message || 'Unknown error'}`);
+      }
+
+      const data = await response.json();
+      console.log('Token:', data);
+      return data;
+    } catch (err) {
+      console.error('Error en la petición fetch:', err);
+      throw err;
     }
   }
+
+  // async getToken() {
+  //   const url = `${this.configService.get<string>('URL_BACKEND')}/auth-bop/get-token`;
+
+    
+  //   const body = {
+  //     credential: {
+  //       client: this.configService.get<string>('Client'),
+  //       secret: this.configService.get<string>('Secret'),
+  //     },
+  //     data: {
+  //       orderId: 'ORD-20250529-001',
+  //       totalAmount: 150000,
+  //       items: [
+  //         {
+  //           sku: 'SKU-001',
+  //           name: 'Zapatos',
+  //           price: 75000,
+  //           quantity: 2,
+  //         },
+  //         {
+  //           sku: 'SKU-002',
+  //           name: 'carro',
+  //           price: 25000,
+  //           quantity: 1,
+  //         },
+  //       ],
+  //       callbackUrl: 'rrrrr',
+  //       redirectionUrl: 'tttttt',
+  //     },
+  //   };
+
+  //   console.log('Enviando credenciales:', body);
+  //   console.log('URL:', url);
+
+  //   try {
+  //     const response = await firstValueFrom(
+  //       this.httpService.post(url, body, {
+  //         headers: {
+  //           'Content-Type': 'application/json',
+  //           'Accept': 'application/json',
+  //         },
+  //         timeout: 8000,
+  //       }),
+  //     );
+
+  //     console.log('Token recibido:', response.data);
+  //     return response.data;
+  //   } catch (error) {
+  //     const status = error.response?.status;
+  //     const data = error.response?.data;
+
+  //     console.error('Error al obtener token:', {
+  //       status,
+  //       data,
+  //       message: error.message,
+  //     });
+
+  //     throw new Error(
+  //       `Fallo al obtener token: ${status || ''} ${
+  //         data?.message || error.message
+  //       }`,
+  //     );
+  //   }
+  // }
 }
